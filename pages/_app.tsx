4c185251ba9a9874fac0791bde96f0b989a8acd8
Jim Burch <jrburch1@gmail.com>
2022-12-20 18:35:12 -0700

    material ui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "../styles/globals.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
